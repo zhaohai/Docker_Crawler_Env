7a659b31afad9c5ef98c202994a9426f380fb3c5
ZhaoHai <33358282@qq.com>
2015-07-09 17:28:37 +0800

    Update start_crawler.sh

--- start_crawler.sh
+++ start_crawler.sh
@@ -4,5 +4,5 @@ emulator -avd test -system system.img -no-window -no-audio -no-skin -verbose > e
 mkdir /root/Downloads
 
 # start 
-java -jar inapp-crawler-1.0.jar /opt/android/android-sdk-linux
+java -jar /publish/inapp-crawler-1.0.jar /opt/android/android-sdk-linux
 
