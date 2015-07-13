bed438627ada01539a1dbd3972002d64876d1744
ZhaoHai <33358282@qq.com>
2015-07-13 11:03:29 +0800

    Update install_xposed.sh

--- install_xposed.sh
+++ install_xposed.sh
@@ -6,7 +6,8 @@ set -x
 # Start emulator
 emulator -avd test -no-window -no-audio -no-skin -verbose > emulator_log &
 adb wait-for-device
- 
+sleep 10
+
 # Download and install Xposed
 wget http://dl-xda.xposed.info/modules/de.robv.android.xposed.installer_v33_36570c.apk -O xposed.apk
 adb install xposed.apk
@@ -33,29 +34,31 @@ adb shell chown root:shell /system/bin/app_process
  
 adb shell sync
 adb shell mount -o remount,ro /system
- 
+
 # Install XposedBridge.jar & modules.list
 adb push xposed/assets/XposedBridge.jar /data/data/de.robv.android.xposed.installer/bin
 adb shell touch /data/data/de.robv.android.xposed.installer/conf/modules.list
- 
-XPOSED_UID=`adb shell dumpsys package de.robv.android.xposed.installer | grep userId | sed -E "s/.*userId\=([0-9]+).*/\1/"`
-adb shell chown $XPOSED_UID:$XPOSED_UID /data/data/de.robv.android.xposed.installer/bin/XposedBridge.jar /data/data/de.robv.android.xposed.installer/conf/modules.list
- 
- 
-# Save system image
-SYSTEM_IMAGE=`grep "[Mm]apping 'system'" emulator_log | awk '{print $NF}'`
-cp $SYSTEM_IMAGE system.img
- 
- 
+
 # Install xposed module
 adb install ActivityInNewTask.apk
 echo "<?xml version='1.0' encoding='utf-8' standalone='yes' ?>" > enabled_modules.xml
 echo '<map> <int name="com.germainz.activityforcenewtask" value="1" /> </map>' >> enabled_modules.xml
 adb push enabled_modules.xml /data/data/de.robv.android.xposed.installer/shared_prefs
 adb shell chown $XPOSED_UID:$XPOSED_UID /data/data/de.robv.android.xposed.installer/shared_prefs/enabled_modules.xml
+adb shell "echo /data/app/com.germainz.activityforcenewtask-1.apk > /data/data/de.robv.android.xposed.installer/conf/modules.list"
  
+XPOSED_UID=`adb shell dumpsys package de.robv.android.xposed.installer | grep userId | sed -E "s/.*userId\=([0-9]+).*/\1/"`
+adb shell chown $XPOSED_UID:$XPOSED_UID /data/data/de.robv.android.xposed.installer/bin/XposedBridge.jar /data/data/de.robv.android.xposed.installer/conf/modules.list
+ 
+# Disable screen lock
+adb shell sqlite3 /data/system/locksettings.db "UPDATE locksettings SET value = '1' WHERE name = 'lockscreen.disabled'"
+ 
+# Sync filesystem
 adb shell sync
  
+# Save system image
+SYSTEM_IMAGE=`grep "[Mm]apping 'system'" emulator_log | awk '{print $NF}'`
+cp $SYSTEM_IMAGE system.img
  
 # Stop emulator
 adb emu kill
